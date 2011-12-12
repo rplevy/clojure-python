d651d4d47bc6d268c3609e8feca31e125613a362
Robert Levy <r.p.levy@gmail.com>
2011-12-12 03:26:11 -0500

    upgrade to Clojure 1.3, use now-available up-to-date Jython 2.5.2, and remove dev-dependencies

--- project.clj
+++ project.clj
@@ -1,7 +1,5 @@
-(defproject clojure-python "0.0.4"
+(defproject clojure-python "0.1.0"
   :description "Improve seamlessness of Clojure Jython interop."
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.python/jython "2.5.1"]]
-  :dev-dependencies [[lein-clojars "0.5.0"]
-                     [swank-clojure "1.2.1"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.python/jython-standalone "2.5.2"]]
   :main clojure-python.core)
