8c709cd0bd6a2928674e5f9fba55ce9a6db0dbed
Robert P. Levy <r.p.levy@gmail.com>
2012-10-25 23:49:16 -0400

    update version for clojars deploy

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-python "0.2.0"
+(defproject clojure-python "0.3.0"
   :description "Improve seamlessness of Clojure Jython interop."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.python/jython-standalone "2.5.2"]]
