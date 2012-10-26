6a94716242d995cb3bbe4a5fed12c3343f90e8a2
Robert P. Levy <r.p.levy@gmail.com>
2012-10-26 03:26:16 -0400

    must use standalone or standard libs do not get included

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject clojure-python "0.4.0"
+(defproject clojure-python "0.4.1"
   :description "Improve seamlessness of Clojure Jython interop."
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.python/jython "2.5.3"]]
+                 [org.python/jython-standalone "2.5.3"]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :profiles {:dev {:dependencies [[midje "1.4.0"]]}}
